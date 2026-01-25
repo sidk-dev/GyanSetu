b08883ceb4e32a62e689c20857940cb949447e75
Siddharth <sidkumar1525@gmail.com>
2026-01-25 22:00:43 +0530

    Improve admin structure of User model

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -1,13 +1,103 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from skills.models import Skill
 from .models import User
 
-class SkillInline(admin.StackedInline):
+class SkillInline(admin.TabularInline):
     model = Skill
     extra = 3
 
-class UserAdmin(admin.ModelAdmin):
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
     inlines = [SkillInline]
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
+    list_display = (
+        "id",
+        "email",
+        "first_name",
+        "last_name",
+        "gender",
+        "region",
+        "district",
+        "credits",
+        "is_active",
+        "is_staff",
+        "is_blocked",
+        "date_joined",
+    )
+
+    list_filter = (
+        "is_active",
+        "is_staff",
+        "is_blocked",
+        "gender",
+        "region",
+        "date_joined",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined", "last_login")
+
+    fieldsets = (
+        ("Login Info", {
+            "fields": ("email", "password")
+        }),
+        ("Personal Info", {
+            "fields": (
+                "first_name",
+                "last_name",
+                "gender",
+                "date_of_birth",
+                "bio",
+            )
+        }),
+        ("Location", {
+            "fields": (
+                "region",
+                "district",
+            )
+        }),
+        ("Credits", {
+            "fields": ("credits",)
+        }),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_blocked",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        ("Important Dates", {
+            "fields": (
+                "last_login",
+                "date_joined",
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email",
+                "first_name",
+                "last_name",
+                "gender",
+                "date_of_birth",
+                "password1",
+                "password2",
+                "is_active",
+                "is_staff",
+            ),
+        }),
+    )
